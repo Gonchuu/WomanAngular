a4484f9f43e1566f7851cd7595f91c950abe8c04
vero7bg7 <vero7bg7@hotmail.com>
2022-07-20 23:11:44 +0200

    creado modelo woman-list, pendiente interfaz IWoman

---
+++ WomanScience/src/app/pages/woman-list/models/woman.models.ts
@@ -0,0 +1,10 @@
+export interface IWoman {
+    createdAt: string;
+    name: string;
+    birth:  string;
+    title:  string;
+    image:  string;
+    phrase:  string;
+    discoveries:  string;
+    id: string;
+}
